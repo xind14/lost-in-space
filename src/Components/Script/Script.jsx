665b3dd3423ce13b7083630a3c5d7c179fd76745
Xin Deng <xind1470@gmail.com>
2024-03-16 00:32:09 -0700

    added script links to render on page

--- src/Components/Script/Script.jsx
+++ src/Components/Script/Script.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from "./script.module.css";
-
+import styles from './script.module.css';
 
 function Script({ character }) {
   // if (!character) return null;
@@ -9,10 +8,17 @@ function Script({ character }) {
     <div className={styles.script}>
       <h1>Welcome {character.actor}!</h1>
       <div className={styles.content}>
-      <h2>Your Character Script</h2>
-      <p>Script: {character.script}</p>
-
-
+        <h2>Your Character Script</h2>
+        {/* <p>Script: {character.script}</p> */}
+        {/* Add Google Docs iframe */}
+        <iframe
+          title="Character's Script"
+          src={character.google_docs_link}
+          width="100%"
+          height="600"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );
